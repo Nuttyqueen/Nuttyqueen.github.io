c8d4fcf1b8d83194fa92f735fa07a53e2ba4f93a
Nuttyqueen <pair25525@gmail.com>
2023-12-08 22:30:22 +0700

    V.1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 20s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }
+    },
+    fontFamily:{
+      'Prompt':['sans-serif']
+    }
+  },
+  plugins: [],
+}
